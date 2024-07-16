db14bcf28b7c4e06c5ea1fc7bf927bb7cfd04440
user <user@usernoMacBook-Air.local>
2024-07-16 13:26:24 +0900

    commit2

--- cook/urls.py
+++ cook/urls.py
@@ -5,4 +5,6 @@ app_name='cook'
 
 urlpatterns=[
     path('',views.IndexView.as_view(),name='index'),
+    path('upload/',views.CreateView.as_view(),name='upload'),
+    path('detail/<int:pk>',views.DetailView.as_view(),name='detail'),
 ]
--- cook/views.py
+++ cook/views.py
@@ -1,6 +1,15 @@
 from django.views import generic
+from django.urls import reverse_lazy
+from .forms import PostCreateForm
 from .models import Post
 
 class IndexView(generic.ListView):
     model=Post
     
+class CreateView(generic.CreateView):
+    model=Post
+    form_class=PostCreateForm
+    success_url=reverse_lazy('cook:index')
+    
+class DetailView(generic.DetailView):
+    model=Post
