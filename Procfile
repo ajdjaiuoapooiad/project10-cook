3b158d114f1a9b6fb258721c83d53894097de5cb
user <user@usernoMacBook-Air.local>
2024-07-17 18:26:04 +0900

    default

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn project10.wsgi
--- project10/settings.py
+++ project10/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,4 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
